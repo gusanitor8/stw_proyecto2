a3173d9b4862a8ce54cbbfa841af194193f6f24d
María Marta Ramírez Gil <ram21342@uvg.edu.gt>
2023-05-25 12:32:03 -0600

    animacion UvIndex medidor

--- src/components/UvWidget.js
+++ src/components/UvWidget.js
@@ -2,9 +2,19 @@ import React from "react";
 import './UvWidget.css';
 
 function UvWidget({uvIndex}) {
+    const meterStyle = {
+        transform: `rotate(${(uvIndex / 10) * 180}deg)`,
+      };
+
     return (
-        <div className="uv-widget-container">
-            <p>UV Index: {uvIndex}</p>
+    <div className="uv-widget-container">
+        <div class="gauge">
+        <div class="gauge__body">
+            <div class="gauge__fill" style={meterStyle}></div>
+            <div class="gauge__cover"></div>
+        </div>
+        </div>
+        <p>UV Index: {uvIndex}</p>
         </div>
     );
 }
